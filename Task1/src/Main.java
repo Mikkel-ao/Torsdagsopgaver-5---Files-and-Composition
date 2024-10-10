e4efb46ffb87a3ac80df22f28a9c9b5b1a49ebd1
mikkel.ao <mikkel.ao.dev@gmail.com>
2024-10-10 09:43:15 +0200

    Completed task1

---
+++ Task1/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1.e
+        ArrayList customers = new ArrayList();
+        Customer Hans = new Customer("Hans", "Hansen", "Hansi");
+        Customer Grete = new Customer("Grete", "Gretesen", "Gertrud");
+        customers.add(Hans);
+        customers.add(Grete);
+
+        // 1.f call to print
+        printCustomers(customers);
+    }
+    // 1.f
+    public static void printCustomers(ArrayList customers) {
+        for (int i = 0; i < customers.size(); i++) {
+            System.out.println(customers.get(i));
+        }
+    }
+
+}
